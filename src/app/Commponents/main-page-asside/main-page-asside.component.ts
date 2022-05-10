16c36c5629becdee2683eff24ac0b57fab8484ed
PetarK37 <komordzic.sr37.2020@uns.ac.rs>
2022-05-11 00:22:42 +0200

    added mainPage

---
+++ src/app/Commponents/main-page-asside/main-page-asside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-asside',
+  templateUrl: './main-page-asside.component.html',
+  styleUrls: ['./main-page-asside.component.css']
+})
+export class MainPageAssideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './Commponents/navigation-bar/navigation-bar.component';
+import { IonicModule } from '@ionic/angular';
+import { MainPagePostsComponent } from './Commponents/main-page-posts/main-page-posts.component';
+import { PostCardComponent } from './Commponents/post-card/post-card.component';
+import { MainPageAssideComponent } from './Commponents/main-page-asside/main-page-asside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    MainPagePostsComponent,
+    PostCardComponent,
+    MainPageAssideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
